56a81a831767bfe622fa6020bea3b9a3488ba3d9
Divakar Pal <31034949+Shaksh@users.noreply.github.com>
2020-03-21 19:55:41 +0530

    Add files via upload

---
+++ ObjectClass/ObjectClass.java
@@ -0,0 +1,13 @@
+/*
+Object class is a class of java which defined in java.lang package.
+Each and every class is directly or indirectly drived class of Object class.
+Object class is root class of each class.
+*/
+
+class ObjectClass
+{
+	public static void main(String a[])
+	{
+		
+	}
+}
